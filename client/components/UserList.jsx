cc225f5a96d4af83e74bc04d74f391e4461220e5
David Cheng <dcheng526@gmail.com>
2020-04-16 12:12:42 -0400

    testing deploy

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -4,7 +4,7 @@ import User from './User.jsx'
 export default ({ users }) => (
   <div className="user-list">
 
-    <p className='lead'>Here's a list of {users.length} completely fictitious people heroku!:</p>
+    <p className='lead'>Here's a list of {users.length} completely fictitious cool people heroku!:</p>
 
     <table className="table">
       <tbody>
